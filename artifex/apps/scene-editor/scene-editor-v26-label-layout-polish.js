251b73fd9a6d35817bd99e83641a04dce173c761
Cinaedvsstudios <cinaedvsstudios@gmail.com>
2026-05-24 23:46:40 +0200

    Delete paused v26 label polish helper

--- artifex/apps/scene-editor/scene-editor-v26-label-layout-polish.js
+++
@@ -1,5 +0,0 @@
-(() => {
-  'use strict';
-  window.ArtifexSceneEditorPatchStatus = window.ArtifexSceneEditorPatchStatus || [];
-  window.ArtifexSceneEditorPatchStatus.push('v26 label layout polish paused for core consolidation');
-})();
