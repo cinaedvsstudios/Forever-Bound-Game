18dfe776a77867ef6724ea6af2f4fd84b5ff9a07
Cinaedvsstudios <cinaedvsstudios@gmail.com>
2026-05-28 05:22:02 +0200

    Add Scene Editor config module

---
+++ artifex/apps/scene-editor/scene-editor-config.js
@@ -0,0 +1,25 @@
+(() => {
+  'use strict';
+
+  const VERSION = 'v0.29-title-cache-sync';
+  const SETTINGS_KEY = 'artifex.sceneEditor.settings.v1';
+  const WORKING_COPY_KEY = 'artifex.sceneEditor.workingCopy.v1';
+  const DOWNLOAD_KEY = 'artifex.sceneEditor.lastDownload.v1';
+  const repoPrefix = location.pathname.includes('/Forever-Bound-Game/') ? '/Forever-Bound-Game/' : '/';
+  const brandLogo = '../../artifexlogo.png';
+  const brandTitle = '../../artifextitle.png';
+  const templateManifest = '../../templates/templates.json';
+  const typeOptions = ['prop', 'pickup', 'player_start', 'npc', 'foe', 'door', 'exit', 'overlay', 'background_layer', 'foreground_layer', 'hazard', 'searchable', 'marker', 'effect', 'ui'];
+
+  window.ArtifexSceneEditorConfig = Object.freeze({
+    VERSION,
+    SETTINGS_KEY,
+    WORKING_COPY_KEY,
+    DOWNLOAD_KEY,
+    repoPrefix,
+    brandLogo,
+    brandTitle,
+    templateManifest,
+    typeOptions
+  });
+})();
