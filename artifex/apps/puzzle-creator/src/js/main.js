700c56f5798f6dbfc1bea9e6d75e4f42e9bce2d6
Cinaedvsstudios <cinaedvsstudios@gmail.com>
2026-06-11 01:16:28 +0200

    x

--- artifex/apps/puzzle-creator/src/js/main.js
+++ artifex/apps/puzzle-creator/src/js/main.js
@@ -1,5 +1,3 @@
 import './engines/maze-labyrinth-runtime.js';
-import './engines-ui.js?v=1.37';
+import './engines-ui.js?v=1.36';
 import './engines/potion-match-runtime.js?v=1.37';
-import './engines/obstacle-course-runtime.js?v=1.39';
-import './engines/obstacle-course-asset-debug.js?v=1.2';
