37fe0d564c54d38c1d8811ee2bc9180446dd82e6
Cinaedvsstudios <cinaedvsstudios@gmail.com>
2026-06-14 22:15:09 +0200

    Bump obstacle course boot to V3.0.12

--- artifex/apps/puzzle-creator/obstacle-course/src/js/main.js
+++ artifex/apps/puzzle-creator/obstacle-course/src/js/main.js
@@ -1,5 +1,5 @@
-const BOOT_VERSION = 'V3.0.11';
-const BOOT_CACHE_VERSION = '3.0.11';
+const BOOT_VERSION = 'V3.0.12';
+const BOOT_CACHE_VERSION = '3.0.12';
 
 const panel = document.querySelector('.right-panel') || document.body;
 const card = document.createElement('article');
