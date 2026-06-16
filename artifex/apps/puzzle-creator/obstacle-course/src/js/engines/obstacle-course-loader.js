f002a8c70b41a4bd0fd56cd6177771124b4a9be9
Cinaedvsstudios <cinaedvsstudios@gmail.com>
2026-06-16 16:29:58 +0200

    Bump loader asset import for V3.0.40

--- artifex/apps/puzzle-creator/obstacle-course/src/js/engines/obstacle-course-loader.js
+++ artifex/apps/puzzle-creator/obstacle-course/src/js/engines/obstacle-course-loader.js
@@ -1,5 +1,5 @@
 import { OC } from './obstacle-course-state.js';
-import { ASSETS, requiredAssetList, optionalAssetList } from './obstacle-course-assets.js?v=3.0.39';
+import { ASSETS, requiredAssetList, optionalAssetList } from './obstacle-course-assets.js?v=3.0.40';
 
 const OPTIONAL_LOAD_CONCURRENCY = 4;
 
