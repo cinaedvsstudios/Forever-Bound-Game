5e3d1fefd9790c3bc62b7f8a958771b5330c67c3
Cinaedvsstudios <cinaedvsstudios@gmail.com>
2026-06-16 16:56:52 +0200

    Bump obstacle course boot for new scatter assets

--- artifex/apps/puzzle-creator/obstacle-course/src/js/main.js
+++ artifex/apps/puzzle-creator/obstacle-course/src/js/main.js
@@ -1,5 +1,5 @@
-const BOOT_VERSION = 'V3.0.40';
-const BOOT_CACHE_VERSION = '3.0.40';
+const BOOT_VERSION = 'V3.0.41';
+const BOOT_CACHE_VERSION = '3.0.41';
 
 const panel = document.querySelector('.right-panel') || document.body;
 const card = document.createElement('article');
