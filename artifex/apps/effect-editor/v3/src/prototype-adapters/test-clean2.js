28867a74b38c5c4d773ef055a09a1fdeca39cf9b
Cinaedvsstudios <cinaedvsstudios@gmail.com>
2026-06-17 23:38:26 +0200

    Test source imports

---
+++ artifex/apps/effect-editor/v3/src/prototype-adapters/test-clean2.js
@@ -0,0 +1,3 @@
+import { SHIMMER_PRESETS, clonePreset } from '../../../fx-shimmer/src/presets.js?v=1.40';
+import { ShimmerDistortionEngine } from '../../../fx-shimmer/src/shimmer-engine.js?v=1.40';
+export { SHIMMER_PRESETS, clonePreset, ShimmerDistortionEngine };
