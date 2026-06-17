dab6e75b6bcde6c0e242ed41d28381444338d4f7
Cinaedvsstudios <cinaedvsstudios@gmail.com>
2026-06-17 23:44:09 +0200

    Remove test source import file

--- artifex/apps/effect-editor/v3/src/prototype-adapters/test-clean2.js
+++
@@ -1,3 +0,0 @@
-import { SHIMMER_PRESETS, clonePreset } from '../../../fx-shimmer/src/presets.js?v=1.40';
-import { ShimmerDistortionEngine } from '../../../fx-shimmer/src/shimmer-engine.js?v=1.40';
-export { SHIMMER_PRESETS, clonePreset, ShimmerDistortionEngine };
